0818db7fa4caab48bb05acb167129f434db55666
Jeonsohee1122 <wjsthgml040726@naver.com>
2023-04-19 01:01:14 +0900

    for_if_list_index

--- test1.py
+++ test1.py
@@ -1,1 +1,13 @@
-print("test")
+# 10개의 정수값을 받아들인다
+numbers = []
+for i in range(10):
+    num = int(input("정수를 입력하세요: "))
+    numbers.append(num)
+
+# 홀수번 정수의 합을 구한다
+total = 0
+for i in range(len(numbers)):
+    if i % 2 == 1:  # 홀수번째 요소인 경우
+        total += numbers[i]
+
+print("홀수번 정수의 합:", total)
